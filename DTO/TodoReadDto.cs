5018bdb2cd0450d1abfd1ee6de21e64b4d6e319a
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2023-12-15 15:40:34 +0100

    Pre Docker

--- DTO/TodoReadDto.cs
+++ DTO/TodoReadDto.cs
@@ -10,6 +10,6 @@ namespace AspApi.DTO
         public int Id { get; set; }
         public string Title {get; set;}
         public DateTime DueDate { get; set; }
-        // public int Priority { get; set; }
+        public int Priority { get; set; }
     }
 }
